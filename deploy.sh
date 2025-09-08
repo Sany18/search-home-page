faa18e946a53055d922254e5f87f49479e2d44be
Alex Tants <aleks_dev@mlp-co.dev>
2025-09-08 09:58:30 +0300

    Push dev force

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ npm run build
 cp -r dist/* .
 git add .
 git commit -m "Deploy build $(date '+%Y-%m-%d %H:%M:%S')" || echo "No changes to commit."
-git push origin deploy
+git push -f origin deploy
 git checkout main
 
 echo "Done."
